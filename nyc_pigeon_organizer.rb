a689616c62b7f32809c7f131ac042b77aa197397
Kevin Clark <kevin.clark93@gmail.com>
2020-02-11 02:21:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+ require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # pp data
+  # start with a hash where keys are pigeon attributes and values are arrays of names who have that attribute
+  # end with a hash where keys are pigeon names and values are arrays of their attributes
+  # keys are all symbols, so need to convert to strings and vice versa
+  
+  # need to reduce to a new hash in the new format
+  # look through each attribute key in the dataset. each of it's keys have a value of an array of names
+  # need to compile an array of unique names found in 
+  pigeon_names = {}
+  data.each{ |key, value|
+    value.each { |key, name_arr|
+      name_arr.each { |name|
+        pigeon_names[name] = {color: [], gender: [], lives: []}
+      }
+      
+    }
+  }
+  
+  pigeon_names.each{ |birdname, attributes_hash|
+    attributes_hash.each { |attribute, arr|
+      
+    }
+  }
+  pp pigeon_names 
 end
